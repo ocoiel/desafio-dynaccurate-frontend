f4e5e170fb8bab140719ae40c5c2ced242a2dbe5
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2023-05-10 18:43:05 -0300

    feat!: create a modal to preview medicament

--- app/@modal/(.)medicament/[id]/page.tsx
+++ app/@modal/(.)medicament/[id]/page.tsx
@@ -1,25 +1,65 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import Image from "next/image"
+import Link from "next/link"
 
-import { Medicaments, medSchema } from "@/types/medicament-schema"
+import { Medicaments } from "@/types/medicament-schema"
 import { fetcher } from "@/lib/fetcher"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog"
 
-export default function Medicament({ params }: { params: { id: string } }) {
-  const [med, setMed] = useState<Medicaments>()
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetcher<Medicaments>(`/api/medicament/${params.id}`)
-      setMed(data)
-    }
-  }, [params.id])
+export default async function Medicament({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const med = await fetcher<Medicaments>(
+    `http://127.0.0.1:3333/med/${params.id}`
+  )
 
   return (
-    <div>
-      amor
-      <h1>oi {params.id}</h1>
-      <p>{med && med.name}</p>
-    </div>
+    <Dialog open={true}>
+      <DialogContent className="sm:px-10 sm:py-8">
+        <DialogHeader>
+          <DialogTitle>Medicamento - {med?.name}</DialogTitle>
+          <DialogDescription>
+            Aqui vai uma descricao qualquer do medicamento.
+            <Link href={"/"}>VOLTAR TESTE</Link>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <h3 className="text-lg font-semibold ">Nome: </h3>
+            <span className="text-base text-foreground">{med?.name}</span>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <h3 className="text-lg font-semibold ">Preço: </h3>
+            <span className="text-base text-foreground">{med?.price}</span>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <h3 className="text-lg font-semibold ">Data de validade: </h3>
+            <span className="text-base text-foreground">
+              {new Date(med?.expiration_date).toLocaleDateString("pt-BR")}
+            </span>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <h3 className="text-lg font-semibold ">Imagem: </h3>
+            <Image src={med.image_url!} fill={true} alt={med?.name} />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button>
+            <Link href={`/medicament/${med?.id}/`}>Ver mais detalhes</Link>
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
